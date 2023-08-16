5e9e74a16d38bcf316fbf7c3281575dddcbb088b
shubhzhs <shubham1003683@gmail.com>
2023-08-16 10:26:32 +0530

    added navigation

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import UsersList from "../components/UsersList";
+import ProfileImage from "./IMG-20210514-WA0006.jpg";
 
 const Users = () => {
   const USERS = [
     {
       id: "u1",
       name: "Shubh Sonawane",
-      image:
-        "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80",
+      image: ProfileImage,
       places: 3,
     },
   ];
